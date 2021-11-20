546131b7ecaa4a1be9c108fbf77166ec89cdd771
antonio cortes <antonio@omegaortega.com>
2021-11-19 20:26:38 -0700

    implementing click listener to respont tpo the reclyclerView

--- app/src/main/java/com/example/movies/ui/CustomAdapter.kt
+++ app/src/main/java/com/example/movies/ui/CustomAdapter.kt
@@ -9,12 +9,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.movies.R
 import com.squareup.picasso.Picasso
 
+fun <T : RecyclerView.ViewHolder> T.listen(event: (position: Int, type: Int) -> Unit): T {
+    itemView.setOnClickListener {
+        event.invoke(getAdapterPosition(), getItemViewType())
+    }
+    return this
+}
+
 class CustomAdapter(private val mList: List<ItemsViewModel>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
     var img_URL_Backdrop = "https://image.tmdb.org/t/p/w300"
+    var listener: MyCustomObjectListener? = null
+
+    fun setCallBackListener(listener:MyCustomObjectListener){
+        this.listener = listener
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.card_view_design, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view).listen { pos, type ->
+            val item = mList.get(pos)
+            listener?.onClickMovie(pos)
+        }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
--- app/src/main/java/com/example/movies/ui/MainActivity.kt
+++ app/src/main/java/com/example/movies/ui/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.movies.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.movies.App
@@ -12,13 +13,19 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),MyCustomObjectListener {
     private val remoteApi = App.remoteApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val data = ArrayList<ItemsViewModel>()
         val adapter = CustomAdapter(data)
+
+        adapter.setCallBackListener(object : MyCustomObjectListener {
+            override fun onClickMovie(id:Int) {
+                print(id)
+            }
+        })
 
         val reclyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         reclyclerView.layoutManager = LinearLayoutManager(this)
@@ -36,5 +43,13 @@ class MainActivity : AppCompatActivity() {
         }
         reclyclerView.adapter = adapter
     }
+
+    override fun onClickMovie(id:Int) {
+
+    }
+}
+
+interface MyCustomObjectListener {
+    fun onClickMovie(id:Int)
 }
 
