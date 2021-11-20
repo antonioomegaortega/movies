990e6b62c39d73c9dc6512d25c6bac04e9a329cc
antonio cortes <antonio@omegaortega.com>
2021-11-19 21:26:43 -0700

    Binds the data to the components on the Details Screen

--- app/src/main/java/com/example/movies/ui/DetailsMoviesActivity.kt
+++ app/src/main/java/com/example/movies/ui/DetailsMoviesActivity.kt
@@ -1,8 +1,11 @@
 package com.example.movies.ui
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.movies.R
+import com.squareup.picasso.Picasso
 
 class DetailsMoviesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +16,17 @@ class DetailsMoviesActivity : AppCompatActivity() {
         var date = extras?.getString("date")
         var overview = extras?.getString("overview")
         var img = extras?.getString("img")
-        print("")
+        var txt_name = findViewById<TextView>(R.id.txt_detail_name)
+        var txt_date = findViewById<TextView>(R.id.txt_detail_date)
+        var img_picture = findViewById<ImageView>(R.id.img_detailMovie)
+        var txt_overview = findViewById<TextView>(R.id.txt_detail_overview)
+
+        var img_URL_Backdrop = "https://image.tmdb.org/t/p/w300"
+        val picasso = Picasso.get()
+        picasso.load(img_URL_Backdrop + img)
+            .into(img_picture)
+        txt_name.text = name
+        txt_date.text = date
+        txt_overview.text = overview
     }
 }
